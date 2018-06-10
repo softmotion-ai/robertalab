3a9a2dab142596f78b2066558b09bc6c20939d3c
akshat-khare <akshat.khare08@gmail.com>
2018-06-10 23:30:51 +0530

    added pagination, sorting order in popup, updated roberta.css to have table head of nice color

--- OpenRobertaParent/OpenRobertaServer/staticResources/js/app/roberta/controller/multSim.controller.js
+++ OpenRobertaParent/OpenRobertaServer/staticResources/js/app/roberta/controller/multSim.controller.js
@@ -5,7 +5,7 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
         function(exports, UTIL, PROGLIST,PROG_C, PROGRAM_M,GUISTATE_C, GUISTATE_M, SIM, USER,  $){
     function init(){
         //currently for debugging purpose logged in a default user
-//        debugger;  
+//        debugger;
    //un comment following lines to switch default login on     
 //        USER.login("a","123456",function(result){
 //            if (result.rc === "ok") {
@@ -23,7 +23,7 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
     function showListProg(){
         PROGLIST.loadProgList(function(result){
             if(result.rc === "ok"){
-                $("#mtable").bootstrapTable('destroy'); //refreshing the table 
+                $("#mtable").bootstrapTable('destroy'); //refreshing the table
                 $("#simModal .btn-primary").off('click'); // deleting any click event attached to this button 
                 var dataarr = []; //Array having data to be displayed in table shown
                 var programsparsed =0; //iterator for the synchronously running for loops
@@ -60,10 +60,6 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
                                 field: 'name',
                                 title: 'Program Name',
                                 sortable: true
-                            }, {
-                                field: 'robot',
-                                title: 'Robot Name',
-                                sortable: true
                             },{
                                 field: 'creator',
                                 title: 'Creator',
@@ -84,6 +80,15 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
 //                        $("#simModal .fixed-table-header").css({"background-color": "#B3BFB8"});
                         $("#simModal .btn-primary").on("click",function(){
                             console.log("Selections will be executed");
+                            var selections = $("#mtable").bootstrapTable('getSelections');
+                            var selectedprograms = [];
+                            for(var i=0;i<selections.length;i++){
+                                var tempfind = oriarray.find(function(ele){
+                                    var thisarrm = oriarray;
+                                    return selections[i].name=== ele[0];
+                                });
+                                selectedprograms.push(tempfind);
+                            }
                             console.log('Selections obtained via getSelections: are ' + JSON.stringify($("#mtable").bootstrapTable('getSelections')));
 //                            alert("The following programs would be executed: "+ JSON.stringify($("#mtable").bootstrapTable('getSelections')));
                             var extractedprograms =[];
@@ -95,6 +100,7 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
                                         alert("failed loading program for item "+ i+", check console");
                                         console.log("failed item is ", item);
                                     }
+                                    dat.savedName = item[0];
                                     extractedprograms[i] = dat;
                                     numloadprogs++;
                                     if(numloadprogs===oriarray.length ){
@@ -117,10 +123,10 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
                                                 if (result.rc === "ok") {
                                                     //                    MSG.displayMessage("MESSAGE_EDIT_START", "TOAST", GUISTATE_C.getProgramName());
                                                     extractedprograms[i]["result"] = result;
-                                                    console.log("added result for ", i);
+//                                                    console.log("added result for ", i);
                                                     programsfetched++;
                                                     if(programsfetched === oriarray.length){
-                                                        console.log("reached end");
+//                                                        console.log("reached end");
                                                         
                                                         console.log(extractedprograms);
                                                         simulateMultiple(extractedprograms);
@@ -141,7 +147,7 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
                                                     alert("failed loading js for item "+i+" check console");
                                                     console.log("failed loading js for item ", item);
                                                 }
-                                                console.log("ran fetching for", i);
+//                                                console.log("ran fetching for", i);
 //                                                PROGRAM_C.reloadProgram(result);
                                             });
                                             
@@ -151,7 +157,7 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
                             });
                         });
                     }
-                });  
+                });
             }else{
                 // the user is not logged in, this code might never be executed until the user deletes the disabled class and click the multiplication button
                 console.log(result.message);
@@ -199,89 +205,6 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
             console.log(result);
             if(result.rc === "ok"){
                 $("#mtable").bootstrapTable('destroy'); //refreshing the table
-                $("#simModal .btn-primary").off('click'); // deleting any click event attached to this button 
-                var dataarr = []; //Array having data to be displayed in table shown
-                var programsparsed =0; //iterator for the synchronously running for loops
-                var robottype = GUISTATE_M.gui.robot;
-                if(result.programNames.length ===0){
-                    $("#voidList").show(); // error reporting in the modal itself
-                    $("#voidList").text("There is no saved program to be simulated. Please save the program and then run this feature"); //error message
-                    $("#simModal .btn-primary").hide(); //hiding the run selected button
-                }else{
-                    $("#voidList").hide(); // hiding the error reporting part
-                    $("#simModal .btn-primary").show(); // showing the run selected button if hidden
-                }
-                result.programNames.forEach(function(item, i, oriarray){
-                    dataarr.push({name: item[0], robot: robottype, creator: item[1], date: item[4]});
-                    programsparsed++;
-                    if(programsparsed===oriarray.length ){ //the end of loop
-                        $('#mtable').bootstrapTable({
-                            height : 400,
-                            sortName: "name",
-                            toggle: "mtable",
-                            iconsPrefix : 'typcn', 
-                            search: true,
-                            icons : {
-                                paginationSwitchDown : 'typcn-document-text',
-                                paginationSwitchUp : 'typcn-book',
-                                refresh : 'typcn-refresh',
-                            },
-                            pagination : 'true',
-                            buttonsAlign : 'right',
-                            resizable : 'true',
-                            
-                            columns: [
-                            {
-                                field: 'name',
-                                title: 'Program Name',
-                                sortable: true
-                            }, {
-                                field: 'robot',
-                                title: 'Robot Name',
-                                sortable: true
-                            },{
-                                field: 'creator',
-                                title: 'Creator',
-                                sortable: true
-                            },{
-                                field: 'date',
-                                title: 'Creation Date',
-                                formatter : UTIL.formatDate,
-                                sortable: true
-                            },{
-                                checkbox : true,
-                                valign : 'middle',
-                            }],
-                            data: dataarr
-                        });
-                        $("#simModal .btn-primary").show();
-                        //the css manipulation has been done in roberta.css
-//                        $("#simModal .fixed-table-header").css({"background-color": "#B3BFB8"});
-                        $("#simModal .btn-primary").on("click",function(){
-                            console.log("Selections will be executed");
-                            console.log('Selections obtained via getSelections: are ' + JSON.stringify($("#mtable").bootstrapTable('getSelections')));
-                            alert("The following programs would be executed: "+ JSON.stringify($("#mtable").bootstrapTable('getSelections')));
-                        });
-                    }
-                });
-            }else{
-                // the user is not logged in, this code might never be executed until the user deletes the disabled class and click the multiplication button
-                console.log(result.message);
-                $("#mtable").bootstrapTable('destroy'); //destroy the table if still present
-                $("#simModal .btn-primary").hide();
-            }
-        });
-    }
-    exports.showListProg = showListProg;
-
-    //deprecated as previously information about the robottype was fetched from the database but it was understood later that it can be directly extracted from guistate.model.js
-    function showListProgOld(){
-        console.log("guistate info is ");
-        console.log(GUISTATE_M.gui.robot);
-        PROGLIST.loadProgList(function(result){
-            console.log(result);
-            if(result.rc === "ok"){
-                $("#mtable").bootstrapTable('destroy'); //refreshing the table
                 var dataarr = []; //Array having data to be displayed in table shown
                 var programsparsed =0; //iterator for the synchronously running for loops
                 result.programNames.forEach(function(item, i, oriarray){
@@ -293,7 +216,7 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
                         var robottype = xmlDocm.documentElement.attributes.robottype.nodeValue; // it is the robot type , say ev3 for some program item
                         dataarr.push({name: item[0], robot: robottype, creator: item[1]});
                         programsparsed++;
-                        if(programsparsed===oriarray.length ){ //the end of loop  
+                        if(programsparsed===oriarray.length ){ //the end of loop
                             $('#mtable').bootstrapTable({
                                 height : 400,
                                 sortName: "name",
@@ -337,7 +260,7 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
                             });
                         }
                     });                     
-                });   
+                }); 
             }else{
                 // the user is not logged in
                 console.log(result.message);
@@ -346,16 +269,5 @@ define(['exports','util', 'progList.model','program.controller', 'program.model'
             }
         });
     }
-<<<<<<< 8b78552bc5ea54b0c11ab956e4c64d0d09fe964e
     exports.showListProgOld = showListProgOld
-<<<<<<< 728a683abf6ba00a73c76e5c03fc92b76fae25f9
-}); 
-    
-  
-    
-=======
-=======
-    exports.showListProg = showListProg
->>>>>>> separate controller added for multiple simulation simulation, the multsim is disabled when not login
 });
->>>>>>> separate controller added for multiple simulation simulation, the multsim is disabled when not login
