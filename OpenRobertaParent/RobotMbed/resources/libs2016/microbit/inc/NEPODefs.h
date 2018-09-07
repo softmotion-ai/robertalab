9ff5f7e9e0e79a5c7b45b075e8b0ef093bd0354d
Reinhard Budde <reinhard.budde@iais.fraunhofer.de>
2018-09-07 15:46:44 +0200

    issue #991 works fpr Calliope2017, now for 2016

--- OpenRobertaParent/RobotMbed/resources/libs2016/microbit/inc/NEPODefs.h
+++ OpenRobertaParent/RobotMbed/resources/libs2016/microbit/inc/NEPODefs.h
@@ -7,3 +7,29 @@
 // To scale them to 0..100 we use this multiplier:
 #define _GET_LIGHTLEVEL_MULTIPLIER 0.3922
 #define _ITERATION_SLEEP_TIMEOUT 1
+
+// math constants used in math blocks. Avoids inclusion of <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#ifndef M_E
+#define M_E 2.718281828459045
+#endif
+
+#ifndef M_GOLDEN_RATIO
+#define M_GOLDEN_RATIO 1.61803398875
+#endif
+
+#ifndef M_SQRT2
+#define M_SQRT2 1.41421356237
+#endif
+
+#ifndef M_SQRT1_2
+#define M_SQRT1_2 0.707106781187
+#endif
+
+#ifndef M_INFINITY
+#define M_INFINITY 0x7f800000
+#endif
