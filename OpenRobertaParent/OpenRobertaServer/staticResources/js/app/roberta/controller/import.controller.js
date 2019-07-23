f01c408d48bf2abb9256980b6bf7c80304f89791
MartinArispeRiveros <martinarisperiveros@gmail.com>
2019-07-23 17:37:40 -0400

    RL-3 Import program from query param
     - Updated name file of external controller
    
     - Added import program from query params

--- OpenRobertaParent/OpenRobertaServer/staticResources/js/app/roberta/controller/import.controller.js
+++ OpenRobertaParent/OpenRobertaServer/staticResources/js/app/roberta/controller/import.controller.js
@@ -78,7 +78,7 @@ define([ 'exports', 'comm', 'message', 'log', 'util', 'guiState.controller', 'pr
             }
         });
     }
-
+    exports.loadProgramFromXML = loadProgramFromXML;
     /**
      * Open a file select dialog to load source code from local disk and send it
      * to the cross compiler
